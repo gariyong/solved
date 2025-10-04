1e7fc0d23b33b46fc37ee9fd3389722d474ec149
gari <happykyk0103@naver.com>
2025-10-04 16:17:14 +0900

    시간 관리

---
+++ baekjoon/greedy/1263.cpp
@@ -0,0 +1,46 @@
+// 巩力 : https://www.acmicpc.net/problem/1263
+// 力格 : 矫埃 包府
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct Homework {
+	int duration;
+	int deadline;
+};
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	vector<Homework> homeworks(n);
+	for (int i = 0; i < n; ++i) {
+		cin >> homeworks[i].duration >> homeworks[i].deadline;
+	}
+	sort(homeworks.begin(), homeworks.end(), [](const Homework& a, const Homework& b) {
+		return a.deadline < b.deadline;
+		});
+
+	int cur_time = homeworks[n - 1].deadline;
+	for (int i = n - 1; i >= 0; i--) {
+		auto& hw = homeworks[i];
+
+		cur_time = min(cur_time, hw.deadline);
+		cur_time -= hw.duration;
+	}
+
+	if (cur_time < 0) {
+		cout << -1;
+	}
+	else {
+		cout << cur_time;
+	}
+
+	return 0;
+}
