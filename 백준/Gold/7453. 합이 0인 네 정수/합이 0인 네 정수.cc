dc279e5293aa34f7a9e2f4d4e9be1ac6735716ee
gariyong <happykyk0103@naver.com>
2026-04-16 15:41:11 +0900

    [Gold II] Title: 합이 0인 네 정수, Time: 7924 ms, Memory: 252184 KB -BaekjoonHub

---
+++ 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n;
+	cin >> n;
+
+	vector<long long> a(n), b(n), c(n), d(n);
+	for (int i = 0; i < n; i++) {
+		cin >> a[i] >> b[i] >> c[i] >> d[i];
+	}
+
+	vector<long long> ab, cd;
+	ab.reserve((long long)n * n);
+	cd.reserve((long long)n * n);
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			ab.push_back(a[i] + b[j]);
+			cd.push_back(c[i] + d[j]);
+		}
+	}
+	sort(cd.begin(), cd.end());
+
+	long long cnt = 0;
+	for (long long x : ab) {
+		auto len = equal_range(cd.begin(), cd.end(), -x);
+		cnt += len.second - len.first;
+	}
+
+	cout << cnt;
+
+	return 0;
+}
