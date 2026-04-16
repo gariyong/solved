c8530ac8ec547ec424ae9a9738f6594ca92ae248
gariyong <happykyk0103@naver.com>
2026-04-16 15:39:43 +0900

    [Gold II] Title: 피보나치 수 6, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+const ll MOD = 1000000007;
+
+vector<vector<ll>> matrix_mult(vector<vector<ll>>& a, vector<vector<ll>>& b) {
+	vector<vector<ll>> result(2, vector<ll>(2, 0));
+
+	for (int i = 0; i < 2; i++) {
+		for(int j = 0; j < 2; j++) {
+			for(int k = 0; k < 2; k++) {
+				result[i][j] = (result[i][j] + a[i][k] * b[k][j]) % MOD;
+			}
+		}
+	}
+
+	return result;
+}
+
+vector<vector<ll>> matrix_pow(vector<vector<ll>>& base, ll exp) {
+	vector<vector<ll>> result = { {1, 0}, {0, 1} };
+
+	while (exp > 0) {
+		if (exp % 2 == 1) {
+			result = matrix_mult(result, base);
+		}
+		base = matrix_mult(base, base);
+		exp /= 2;
+	}
+
+	return result;
+}
+
+int main() {
+	ll n;
+
+	cin >> n;
+
+	if (n == 0) {
+		cout << 1;
+
+		return 0;
+	}
+
+	vector<vector<ll>> base = { {1, 1}, {1, 0} };
+	vector<vector<ll>> result = matrix_pow(base, n - 1);
+
+	cout << result[0][0];
+
+	return 0;
+}
